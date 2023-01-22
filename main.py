16075d8af120336ed2b19833a25fd70e11e258b4
yash kumar <yash.kumar0042@gmail.com>
2023-01-22 08:55:03 +0100

    updated api basic to parameters

--- main.py
+++ main.py
@@ -3,9 +3,21 @@ from flask import Flask, jsonify, request, send_file
 app = Flask(__name__)
 
 @app.route('/')
-
-def hello():
+def welcome():
     return "hello World"
+
+@app.route('/my-first-api', methods = ['GET'])
+def hello():
+
+    name = request.args.get('name')
+
+    if name is None:
+        text = 'Hello!'
+
+    else:
+        text = 'Hello ' + name + '!'
+
+    return text
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
