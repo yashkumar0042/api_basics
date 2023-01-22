f7eae83aebfa79ee59c058e47d7bbb1f2aa81202
yash kumar <yash.kumar0042@gmail.com>
2023-01-22 08:39:53 +0100

    fist api basic

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify, request, send_file
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def hello():
+    return "hello World"
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
